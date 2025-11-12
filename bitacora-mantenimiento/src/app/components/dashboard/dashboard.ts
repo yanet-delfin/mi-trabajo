517aae023d1aa9a675f955262f2e284f11563bcf
FernandaYanetBravoDelfin <yanetdelfin10@gmail.com>
2025-11-12 16:46:19 -0600

    n

--- bitacora-mantenimiento/src/app/components/dashboard/dashboard.ts
+++ bitacora-mantenimiento/src/app/components/dashboard/dashboard.ts
@@ -1,11 +1,29 @@
-import { Component } from '@angular/core';
+import { ChangeDetectorRef, Component } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+import { Mantenimiento } from '../../services/mantenimiento';
+import { firstValueFrom } from 'rxjs';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-dashboard',
-  imports: [],
+  standalone: true,
+  imports: [
+    CommonModule
+  ],
   templateUrl: './dashboard.html',
   styleUrl: './dashboard.css',
 })
 export class Dashboard {
 
+  servicios: any[] = [];
+  constructor(
+    private apiservicio: Mantenimiento,
+    private detectarCambios: ChangeDetectorRef
+  ) { }
+
+  async ngOnInit() {
+    const datos = await firstValueFrom(this.apiservicio.obtenerServicios());
+    this.servicios = datos.data;
+    this.detectarCambios.detectChanges();
+  }
 }
--- bitacora-mantenimiento/src/app/services/mantenimiento.ts
+++ bitacora-mantenimiento/src/app/services/mantenimiento.ts
@@ -1,8 +1,19 @@
+import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
 
 @Injectable({
   providedIn: 'root',
 })
 export class Mantenimiento {
+
+  private apiUrl = 'https://apiservice-aox9.onrender.com/api/servicios';
+
+  constructor(private http: HttpClient) { }
+
+  //1. Obtener todos los servicios
+  obtenerServicios(): Observable<any> {
+    return this.http.get('${this.apiUrl}');
+  }
   
 }
