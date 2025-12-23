5cff3fc9e7d9e4cdf5fdca34af8235accd62e981
FernandaYanetBravoDelfin <yanetdelfin10@gmail.com>
2025-12-22 19:09:26 -0600

    rapprutas

--- asistencia-escolar/src/app/app.routes.ts
+++ asistencia-escolar/src/app/app.routes.ts
@@ -1,3 +1,13 @@
 import { Routes } from '@angular/router';
+import { DashboardComponent } from './components/dashboard-component/dashboard-component';
+import { RegistrarClaseComponent } from './components/registrar-clase-component/registrar-clase-component';
+import { RegistrarAlumnoComponent } from './components/registrar-alumno-component/registrar-alumno-component';
+import { AdminCatalogosComponent } from './components/admin-catalogos-component/admin-catalogos-component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    { path: 'dashboard', component: DashboardComponent },
+    { path: 'craer-clase', component: RegistrarClaseComponent },
+    { path: 'registro-alumnos', component: RegistrarAlumnoComponent },
+    { path: 'configuracion', component: AdminCatalogosComponent },
+    { path: '**', redirectTo: 'dashboard' }
+];
