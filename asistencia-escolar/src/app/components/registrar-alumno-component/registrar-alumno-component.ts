49a3ebfe2f770c8d5b9f2389ed7e8f5e5bc6294a
FernandaYanetBravoDelfin <yanetdelfin10@gmail.com>
2025-12-31 17:04:07 -0600

    registro clases component

--- asistencia-escolar/src/app/components/registrar-alumno-component/registrar-alumno-component.ts
+++ asistencia-escolar/src/app/components/registrar-alumno-component/registrar-alumno-component.ts
@@ -1,11 +1,41 @@
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+import { EscuelaService } from '../../services/escuela-service';
 
 @Component({
   selector: 'app-registrar-alumno-component',
-  imports: [],
+  standalone: true,
+  imports: [
+    CommonModule,
+    FormsModule
+  ],
   templateUrl: './registrar-alumno-component.html',
   styleUrl: './registrar-alumno-component.css',
 })
 export class RegistrarAlumnoComponent {
+
+  alumno = {
+    nombre: '',
+    matricula: '',
+  };
+
+  mensaje = '';
+
+  constructor(private escuelaService: EscuelaService) { }
+
+  async guardarAlumno() {
+    if (!this.alumno.nombre || !this.alumno.matricula) {
+      this.mensaje = 'Por favor, complete todos los campos.';
+      return;
+    }
+    try {
+      await this.escuelaService.registrarAlumno(this.alumno);
+      this.mensaje = 'Alumno registrado exitosamente.';
+      this.alumno = { nombre: '', matricula: '' }; // Reset form
+    } catch (error) {
+      this.mensaje = 'Error al registrar el alumno. Intente nuevamente.';
+    }
+  }
 
 }
